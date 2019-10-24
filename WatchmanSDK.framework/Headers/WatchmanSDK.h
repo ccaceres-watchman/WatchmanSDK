fd850419108a7b4c716cad388b4f087ee27dc12b
Carlos Cáceres Watchman Door <ccaceres@watchman.es>
2019-10-24 11:30:21 +0200

    <v1.5> Added WatchmanSDK.framework.

---
+++ WatchmanSDK.framework/Headers/WatchmanSDK.h
@@ -0,0 +1,19 @@
+//
+//  WatchmanSDK.h
+//  WatchmanSDK
+//
+//  Created by Gabriel Codarcea on 23/7/18.
+//  Copyright © 2018 Gabriel Codarcea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchmanSDK.
+FOUNDATION_EXPORT double WatchmanSDKVersionNumber;
+
+//! Project version string for WatchmanSDK.
+FOUNDATION_EXPORT const unsigned char WatchmanSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchmanSDK/PublicHeader.h>
+
+
